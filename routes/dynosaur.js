82d4b56f1ea6f927053d3c25d34089436e819633
wi1dcode <magosdk@icloud.com>
2022-09-05 15:39:52 +0200

    put and delete method done

--- routes/dynosaur.js
+++ routes/dynosaur.js
@@ -16,6 +16,7 @@ app.get("/", async (req, res) => {
   try {
     const dynosaurs = await Dynosaur.findAll({
       attributes: [
+        "id",
         "name",
         "scientific",
         "appearance",
@@ -23,6 +24,59 @@ app.get("/", async (req, res) => {
         "description",
         "color",
       ],
+    })
+
+    res.json(dynosaurs)
+  } catch (e) {
+    console.log(e)
+    res.status(500).json("Internal server error")
+  }
+})
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const dynosaurs = await Dynosaur.findOne({
+      where: {
+        id,
+      },
+    })
+
+    if (dynosaurs) {
+      res.json(dynosaurs)
+    } else {
+      res.status(404).json("Dynosaur not found")
+    }
+  } catch (e) {
+    console.log(e)
+    res.status(500).json("Internal server error")
+  }
+})
+
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Dynosaur.destroy({
+      where: { id },
+    })
+
+    res.status(204).json("Deleted")
+  } catch (e) {
+    console.log(e)
+    res.status(500).json("Internal server error")
+  }
+})
+
+app.put("/:id", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const dynosaurs = await Dynosaur.update(req.body, {
+      where: {
+        id,
+      },
     })
 
     res.json(dynosaurs)
